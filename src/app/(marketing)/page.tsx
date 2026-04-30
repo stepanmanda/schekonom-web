3b45eb560943bf9d26d785cc8ef537d26ea51bff
Štěpán Manda <stepan@Stepan--MacBook-Air.local>
2026-04-30 04:43:29 +0200

    auto: Edit on page.tsx

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -2,6 +2,7 @@ import HeroSection from "@/components/marketing/HeroSection";
 import WhatIsItSection from "@/components/marketing/WhatIsItSection";
 import StoriesSection from "@/components/marketing/StoriesSection";
 import HiddenSignalsDemo from "@/components/marketing/HiddenSignalsDemo";
+import LockedTeaser from "@/components/marketing/LockedTeaser";
 import ICPSection from "@/components/marketing/ICPSection";
 import AboutSection from "@/components/marketing/AboutSection";
 import ContactSection from "@/components/marketing/ContactSection";
@@ -14,6 +15,7 @@ export default function HomePage() {
       <WhatIsItSection />
       <StoriesSection />
       <HiddenSignalsDemo />
+      <LockedTeaser />
       <ICPSection />
       <AboutSection />
       <ContactSection />
